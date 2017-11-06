e73e8c86a58c35d11a9ea23cc1daebd85ee5ee19
Myung Suh Son <myungsuhson@gmail.com>
2017-11-05 19:04:00 -0700

    hooray I did it

---
+++ Practice1.rb
@@ -0,0 +1,11 @@
+print "How old are you?"
+age_person = gets.to_i
+
+print "How old is your mother?"
+age_person_mother = gets.to_i
+
+print "How old is your father?"
+age_person_father = gets.to_i
+
+puts "The sum age of your family is"
+print(age_person + age_person_mother + age_person_father).to_s
